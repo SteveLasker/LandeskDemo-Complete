a36dcc2793dbb90bfcb2768f88c74c4261f3c12a
Steve Lasker <StevenLasker@Hotmail.com>
2015-03-30 16:54:50 -0700

    Added Salesforce
    Added Salesforce Account, Contact and Case objects
    Included views for Contact and Case to test the query and update
    capabilities

---
+++ src/LandeskUMP/LandeskUMP/Controllers/CaseController.cs
@@ -0,0 +1,69 @@
+﻿using Salesforce.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using LandeskUMP.Models.Salesforce;
+using LandeskUMP.Salesforce;
+
+namespace LandeskUMP.Controllers
+{
+    public class CaseController : Controller
+    {
+        // Note: the SOQL Field list, and Binding Property list have subtle differences as custom properties may be mapped with the JsonProperty attribute to remove __c
+        const string _CasePostBinding = "CaseNumber,Subject,Description,Reason,Category,EngineeringReqNumber,Status,Priority,Severity,Product_Line,Product,SLAViolation";
+        /*
+                    Id,
+                    AccountId,
+                    CaseNumber,
+                    Subject,
+                    Description,
+                    Reason,
+                    Category__c,
+                    EngineeringReqNumber__c,
+                    Status,
+                    Priority,
+                    Severity__c,
+                    Product_Line__c,
+                    Product__c,
+                    SLAViolation__c,
+                    CreatedDate,
+                    ClosedDate,
+                    CreatedById,
+                    IsClosed,
+                    IsDeleted
+        */
+
+
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<Case> selectedCases = Enumerable.Empty<Case>();
+            try
+            {
+                selectedCases = await SalesforceService.MakeAuthenticatedClientRequestAsync(
+                    async (client) =>
+                    {
+                        QueryResult<Case> cases =
+                        await client.QueryAsync<Case>("SELECT Id,CaseNumber,Subject,Description,Reason,Category__c,EngineeringReqNumber__c,Status,Priority,Severity__c,Product_Line__c,Product__c,SLAViolation__c,CreatedDate,ClosedDate,CreatedById,IsClosed,IsDeleted FROM Case WHERE Severity__c > ' '");
+                        return cases.records;
+                    }
+                    );
+            }
+            catch (Exception e)
+            {
+                this.ViewBag.OperationName = "query Salesforce Cases";
+                this.ViewBag.AuthorizationUrl = SalesforceOAuthRedirectHandler.GetAuthorizationUrl(this.Request.Url.ToString());
+                this.ViewBag.ErrorMessage = e.Message;
+            }
+            if (this.ViewBag.ErrorMessage == "AuthorizationRequired")
+            {
+                return Redirect(this.ViewBag.AuthorizationUrl);
+            }
+            return View(selectedCases);
+
+        }
+    }
+}
