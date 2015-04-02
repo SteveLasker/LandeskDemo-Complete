aa51c4d05bef400b62c61db6f258ba0f86512c45
Steve Lasker <StevenLasker@Hotmail.com>
2015-04-02 16:21:21 -0700

    Adding VSO phase 1

---
+++ src/LandeskUMP/LandeskUMP/Controllers/VSOController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LandeskUMP.Controllers
+{
+    public class VSOController : Controller
+    {
+        // GET: VSO
+        public ActionResult Index()
+        {
+            LandeskVSO.TFSWorkItemStoreConnector workItems = new LandeskVSO.TFSWorkItemStoreConnector();
+            Microsoft.TeamFoundation.Client.TfsConfigurationServer tfsConfi = workItems.AuthenticateToTFSService();
+
+                
+            return View();
+        }
+    }
+}
